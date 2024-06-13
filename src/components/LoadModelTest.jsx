daa51d9e7884c789b7dead006a91e1ee2f0b4550
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2024-06-13 14:11:51 +0200

    fix: animation controls of the official model v0.5

--- src/components/LoadModelTest.jsx
+++ src/components/LoadModelTest.jsx
@@ -1,18 +1,29 @@
 // GltfViewer.js
 import { useAnimations, useGLTF } from "@react-three/drei";
 import React, { useRef } from "react";
+import { useCharacterAnimations } from "../contexts/CharacterAnimations";
 
 const GltfViewer = () => {
   const group = useRef();
   const { nodes, materials, animations } = useGLTF('./models/SHARA3.gltf');
   const { actions } = useAnimations(animations, group);
+  const { animationIndex, setAnimations } = useCharacterAnimations();
 
   React.useEffect(() => {
-    // Reproduce la primera animación (ajusta según sea necesario)
-    if (actions && Object.keys(actions).length > 0) {
-      actions[Object.keys(actions)[0]].play();
+    if (actions) {
+      const animationNames = Object.keys(actions);
+      console.log("Available animations:", animationNames);
+
+      setAnimations(animationNames);
+
+      if (animationNames.length > 0) {
+        Object.values(actions).forEach((action) => action.stop());
+        if (animationNames[animationIndex]) {
+          actions[animationNames[animationIndex]].play();
+        }
+      }
     }
-  }, [actions]);
+  }, [actions, animationIndex, setAnimations]);
 
   return (
     <group ref={group} dispose={null}>
