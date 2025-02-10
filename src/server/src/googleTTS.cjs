15d018bdc1ba8a9d915fcf17a01f1d58a653dd6d
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2025-02-10 12:43:59 +0100

    fix: tts with correct use of credentials

--- src/server/src/googleTTS.cjs
+++ src/server/src/googleTTS.cjs
@@ -1,48 +1,44 @@
-const axios = require('axios');
-// const ffmpeg = require('fluent-ffmpeg');
-// const fs = require('fs');
+const textToSpeech = require('@google-cloud/text-to-speech');
 require('dotenv').config();
 
-const synthesize = async (req, res) => {
-  const text = req.body.text;
-  const apiKey = process.env.GOOGLE_API_KEY;
-  const endpoint = `https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=${apiKey}`;
-  const payload = {
-    "audioConfig": {
-      "audioEncoding": "LINEAR16",
-      "effectsProfileId": [
-        "medium-bluetooth-speaker-class-device"
-      ],
-      "pitch": 1.6,
-      "speakingRate": 1
-    },
-    "input": {
-      "text": text
-    },
-    "voice": {
-      "languageCode": "es-ES",
-      "name": "es-ES-Standard-C"
-    }
+const getCredentialsConfig = () => {
+  const googleCreds = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+  try {
+    const parsedCreds = JSON.parse(googleCreds);
+    return { credentials: parsedCreds };
+  } catch (e) {
+    return { keyFilename: googleCreds };
   }
+};
 
-  const response = await axios.post(endpoint, payload);
-  // Save the audio content to a file for debbuging purposes
-  // const audio = response.data.audioContent;
-  // const audioBuffer = Buffer.from(audio, 'base64');
+const synthesize = async (req, res) => {
+  try {
+    const credentialsConfig = getCredentialsConfig();
+    const client = new textToSpeech.TextToSpeechClient(credentialsConfig);
+    const text = req.body.text;
 
-  // fs.writeFile('src/tests/output_mono.wav', audioBuffer, (err) => {
-  //   if (err) throw err;
-  //   console.log('Audio content has been saved as output.wav');
-  // });
+    const request = {
+      input: { text: text },
+      voice: {
+        languageCode: 'es-ES',
+        name: 'es-ES-Standard-C'
+      },
+      audioConfig: {
+        audioEncoding: 'LINEAR16',
+        effectsProfileId: ['medium-bluetooth-speaker-class-device'],
+        pitch: 1.6,
+        speakingRate: 1
+      },
+    };
+    const [response] = await client.synthesizeSpeech(request);
+    const audioContent = response.audioContent.toString('base64');
 
-  // ffmpeg('src/tests/output_mono.wav')
-  //   .outputOption('-ac 2')
-  //   .save('src/tests/output_stereo.wav')
-  //   .on('end', () => {
-  //     console.log('Audio content has been saved as output_stereo.wav');
-  //   });
-  // Return the audio content to the client
-  res.json(response.data);
+    res.json({ audioContent });
+  } catch (error) {
+    console.error('Error en la síntesis de voz:', error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = synthesize;
