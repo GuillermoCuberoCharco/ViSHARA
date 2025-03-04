22bc7504ac916dc649584390916c575a4a299801
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2025-03-04 12:34:26 +0100

    fix: prontLoader file location

--- src/server/src/opeanaiService.cjs
+++ src/server/src/opeanaiService.cjs
@@ -1,5 +1,5 @@
 const OpenAI = require('openai');
-const { loadPrompt } = require('./utils/promptLoader.cjs');
+const { loadPrompt } = require('../utils/promptLoader.cjs');
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
