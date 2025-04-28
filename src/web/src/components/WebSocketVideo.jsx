29be92d3e21d12ba6ec509a8e09f4945db52f8b0
GuillermoCuberoCharco <91546381+GuillermoCuberoCharco@users.noreply.github.com>
2025-04-28 12:06:52 +0200

    fix(WebSocketVideo): add socketRef check and condition for requestAnimationFrame
    Ensure transmission stops correctly by checking `socketRef.current` before sending frames and only scheduling `requestAnimationFrame` if transmission is still active. This prevents potential errors and unnecessary frame requests when the socket is closed or transmission is stopped.

--- src/web/src/components/WebSocketVideo.jsx
+++ src/web/src/components/WebSocketVideo.jsx
@@ -132,7 +132,7 @@ const WebSocketVideoComponent = ({ onStreamReady }) => {
         isTransmitting.current = true;
 
         const sendFrame = () => {
-            if (!isTransmitting.current) { console.log("Transmision stopped"); return; }
+            if (!isTransmitting.current || !socketRef.current) { console.log("Transmision stopped"); return; }
 
             const now = Date.now();
             const timeDiff = now - lastFrameTimeRef.current;
@@ -151,7 +151,9 @@ const WebSocketVideoComponent = ({ onStreamReady }) => {
                 }
                 lastFrameTimeRef.current = now;
             }
-            frameRequestRef.current = requestAnimationFrame(sendFrame);
+            if (isTransmitting.current) {
+                frameRequestRef.current = requestAnimationFrame(sendFrame);
+            }
         };
 
         if (video.readyState >= 3) {
